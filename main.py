56fd5b7ce96c187a0550cfa962195476302ec78e
Kristian <Kristian.E64@gmail.com>
2025-11-06 23:00:45 +0100

    Flask project initialized in uv

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from ai-swedish-whisperer!")
+from flask import Flask, render_template
+from logging import FileHandler
+import logging
 
+app = Flask (__name__)
+
+def initialize_logger():
+    logger = FileHandler ('debug.log')
+    app.logger.setLevel (logging.DEBUG)
+    app.logger.addHandler (logger)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    initialize_logger()
+    app.logger.debug ('hello')
+    return render_template ('index.html', prompt_response = 'some response')
+
+def main():
+    app.run (debug = True)
 
 if __name__ == "__main__":
     main()
