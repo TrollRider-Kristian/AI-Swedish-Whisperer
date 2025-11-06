c72f39081b13ba9b0b541d19d3282f4f3dfe6a2e
Kristian <Kristian.E64@gmail.com>
2025-11-07 00:49:43 +0100

    fixed the flow of the LLM, but still not catered to Swedish, and also lockfile

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from logging import FileHandler
 import logging
+import ollama
 
 app = Flask (__name__)
+ollama_client = ollama.Client()
 
 def initialize_logger():
     logger = FileHandler ('debug.log')
@@ -12,8 +14,13 @@ def initialize_logger():
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     initialize_logger()
-    app.logger.debug ('hello')
-    return render_template ('index.html', prompt_response = 'some response')
+    if (request.method == 'GET'):
+        app.logger.debug (request.args.to_dict())
+        if 'user-response' in request.args.to_dict():
+            response = ollama_client.generate ("llama3.1", prompt = request.args.to_dict()['user-response'])
+        else:
+            response = ollama_client.generate ("llama3.1", prompt = "What's the best way to learn Swedish?")
+    return render_template ('index.html', prompt_response = response.response)
 
 def main():
     app.run (debug = True)
